09bf102833b271a85293c3d57abc02657df25e0a
Ralph Nahra <rallph@protonmail.com>
2021-08-11 16:59:53 -0500

    add execute debugee method

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <string>
 #include <unistd.h>
+#include <sys/ptrace.h>
+
+
+void execute_debugee(const std::string& program_name) {
+    if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
+        std::cerr << "Error in ptrace\n";
+        return;
+    }
+    execl(program_name.c_str(), program_name.c_str(), nullptr);
+}
+
 
 int main(int argc, char* argv[]) {
 
