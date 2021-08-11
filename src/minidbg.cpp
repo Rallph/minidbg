243332ea135367a3649b21c1d2342fbe6627f9c2
Ralph Nahra <rallph@protonmail.com>
2021-08-11 17:32:05 -0500

    add debugger run method

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -2,7 +2,12 @@
 #include <string>
 #include <unistd.h>
 #include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <linenoise.h>
 
+#include "debugger.hpp"
+
+using namespace minidbg;
 
 void execute_debugee(const std::string& program_name) {
     if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
@@ -10,6 +15,19 @@ void execute_debugee(const std::string& program_name) {
         return;
     }
     execl(program_name.c_str(), program_name.c_str(), nullptr);
+}
+
+void debugger::run() {
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
+
+    char* line = nullptr;
+    while ((line = linenoise("minidbg> ")) != nullptr) {
+        // handle command
+        linenoiseHistoryAdd(line);
+        linenoiseFree(line);
+    }
 }
 
 
